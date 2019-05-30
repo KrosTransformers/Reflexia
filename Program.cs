c1c17dfc169c0309818f25fda241cc0df4f2e8d8
Kros Transformers <47741344+KrosTransformers@users.noreply.github.com>
2019-05-30 10:34:51 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extended
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var u = new Reader();
+
+            int a = u.Read<int>();           
+            Console.WriteLine($"{a}");
+
+            double v = u.Read<double>();
+            Console.WriteLine($"{v}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Reader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Extended
+{
+    public class Reader
+    {
+        
+        public T Read<T>()
+        {
+            var input = Console.ReadLine();
+            return Parse<T>(input);
+        }
+
+        public T Parse<T>(string input)
+        {
+            T ret = default(T);
+            while (!MyTryParse<T>(input, out ret))
+            {
+                Console.WriteLine("Nesprávny vstup");
+                input = Console.ReadLine();
+            }
+            return ret;
+        }
+
+        public bool MyTryParse<T>(string input, out T result)
+        {
+            result = default(T);
+            object[] par = new object[] { input, result };
+            var ret = typeof(T)
+                .GetMethod("TryParse", new[] { typeof(string), typeof(T).MakeByRefType() })
+                .Invoke(this, par);
+            result = (T)par[1];
+            return (bool) ret;
+        }
+    }
+}
